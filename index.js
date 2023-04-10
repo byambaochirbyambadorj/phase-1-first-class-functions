f8db5bf89e80329ae7038441bedeb497359731c8
Bryan Byambadorj <byambaochir_a@icloud.com>
2023-04-09 20:10:14 -0500

    completed

---
+++ index.js
@@ -0,0 +1,10 @@
+function receivesAFunction (callback) {
+    callback()
+}
+function returnsANamedFunction () {
+    return function nameFunction(){ 
+    }
+}
+function returnsAnAnonymousFunction(){
+    return function (){}
+}
